14fc707d4b7b7fc36ae7658710b8f0819a072cf9
ev-n-er <evgenyerkaev@gmail.com>
2026-09-15 14:54:11 +0700

    Dropped duplicate initialization of Apollo plugin

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { join } from "path";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, type ApolloDriverConfig } from '@nestjs/apollo';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ProfileModule } from './modules/profile/profile.module.ts';
 
 @Module({
@@ -12,7 +11,6 @@ import { ProfileModule } from './modules/profile/profile.module.ts';
       driver: ApolloDriver,
       autoSchemaFile: true,
       graphiql: true,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
   ],
 })
